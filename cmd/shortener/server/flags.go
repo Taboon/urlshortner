724cf97b0022b8ccd3bfa96eb2ff7350a39a33cb
Taboon <gojonik@gmail.com>
2024-03-06 11:09:56 +0300

    add os env variables

--- cmd/shortener/server/flags.go
+++ cmd/shortener/server/flags.go
@@ -12,7 +12,7 @@ func parseFlags() {
 	// как аргумент -a со значением :8080 по умолчанию
 	flag.StringVar(&config.ConfigGlobal.BaseURL, "b", "http://127.0.0.1:8080", "address to make short url")
 	flag.Var(&config.ConfigGlobal.LocalAddress, "a", "address to start server")
-	// парсим переданные серверу аргументы в зарегистрированные переменные
+
 	flag.Parse()
 	if envRunAddr := os.Getenv("SERVER_ADDRESS"); envRunAddr != "" {
 		err := config.ConfigGlobal.LocalAddress.Set(envRunAddr)
