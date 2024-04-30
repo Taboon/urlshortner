cc9f6c3d3243738a7e2893cdf9d99890adcace16
taboon <gojonik@gmail.com>
2024-04-30 20:13:40 +0300

    Add cookies autorisation

--- internal/server/auth/middleware.go
+++ internal/server/auth/middleware.go
@@ -9,7 +9,23 @@ func (a *Autentificator) MiddlewareCookies(h http.HandlerFunc) http.HandlerFunc 
 	auth := func(w http.ResponseWriter, r *http.Request) {
 		var id int
 		ctx := r.Context()
+
+		header := r.Header.Get("Autorization")
+		if header != "" {
+			a.Log.Debug("Достаем ID из хеадера")
+			id = a.readToken(ctx, header)
+			if id == 0 {
+				w.WriteHeader(http.StatusUnauthorized)
+				return
+			}
+			a.Log.Debug("Устанавливаем в контекст", zap.Int("id", id))
+			r = r.WithContext(a.setContext(r.Context(), id))
+			h.ServeHTTP(w, r)
+			return
+		}
+
 		cookie, err := r.Cookie("Authorization")
+
 		if err != nil {
 			a.Log.Debug("Устанавливаем куки")
 			w, id = a.setCookies(ctx, w)
